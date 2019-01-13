c38957ba8af9c2f28d46e9746ea2a773c883e4f0
pskpetya <pskpetya@gmail.com>
2019-01-13 19:52:42 +0400

    need to add only for exceeds

---
+++ js/exceeds.js
@@ -0,0 +1,55 @@
+// li .personFull: add to slick carosele
+// $(document).ready(function(){
+//   const arrowLeft = document.querySelector('#left');
+//   //let personFull = document.querySelectorAll('.personFull');
+//   let li = document.querySelectorAll('.personFull');
+//   const popup = document.querySelector('popup');
+//   let personFull = document.querySelector('.personFull');
+//
+//   left.addEventListener('click', (e)=>{
+//     //console.log(e.target.parentNode);//.popup-content; parent of popup-content-text
+// console.log(personFull);
+//
+//  });
+// search !!!
+// function search(){
+//   var input, filter, ul, li, a, i;
+//   input = document.getElementById("search");
+//   filter = input.value.toUpperCase();
+//   ul = document.getElementById("myUL");
+//   li = document.getElementsByTagName('li');
+//
+//         //names = ul.querySelectorAll('.name');
+//   for (i = 0; i < li.length; i++) {
+//       p = li[i].getElementsByTagName('p')[0];
+//       if (p.textContent.toUpperCase().indexOf(filter) > -1) {
+//           li[i].style.display = "";
+//       } else {
+//           li[i].style.display = "none";
+//       }
+//   }
+//   console.log('proba');
+// }
+// search();  /// SHOOT!! WHY DOESNT WORK????
+
+// another approach of search: create input options here and add datalist
+// option value = $('li.p.name');
+ const input = document.creatElement('input');
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// proba
---
+++ js/main.js
@@ -0,0 +1,74 @@
+    function displayInfo(data){
+        var personList = '<ul id="myUl">';
+        $.each(data.results, function(i, item){
+          let date = item.dob.date.substring(5,7);
+          let month = item.dob.date.substring(8,10);
+          let year = item.dob.date.substring(0,4);
+
+          personList += `<li class="person"><img src = "${item.picture.large}"/>
+          <span><p class="name"> ${item.name.first} ${item.name.last}</p>
+          <p class="email">${item.email}</p>
+          <p class="location">${item.location.city}</p>
+          </span><div class="personExtraData" style="display:none">
+          <p>${item.cell}</p>
+          <p class="address">${item.location.street}, ${item.location.state}</p>
+          <p> ${item.location.postcode}</p>
+          <p>Birthday: ${date}/${month}/${year}</p></div></li>`;
+        });
+        //personList += '<ul>';
+        $('.js').html(personList);
+        // POP UP WINDOW
+        const person = document.querySelectorAll('.person');
+        const close = document.getElementById('close');
+        const popup = document.querySelector('.popup');
+        const popupContentText = document.querySelector('.popup-content-text');
+
+        $.each(person,function(i){
+           person[i].addEventListener('click',(e)=>{
+               popup.style.display = "block";
+
+              let personHTML = $(this).clone();
+              $(popupContentText).html(personHTML);
+               $(personHTML).removeClass('person').addClass('personFull');
+               $(personHTML).children('div').show();
+            //   make the body behind the popup window darker
+               $('ul, .person, body').css('background-color', 'darkgrey');
+              // $('ul, .person, body').fadeTo( 1.2s,.2);
+           });
+        });
+        $('#close').on('click',function(){
+           $(popup).hide();
+           $('.person').css('background-color', 'white');
+            $('body, ul').css('background-color', '#e0ebeb');
+        });
+        // search bar:
+        // function searchEmployee(){
+        //   //.person is li, p is .name
+        //
+        //   const input = document.getElementById('search');
+        //   let filter = input.value.toLowerCase();
+        //   const ul = document.getElementById('myUl');
+        //   let li = ul.getElementsByTagName('li');
+        //   console.log(li[0]);
+        // }
+    }
+
+    $.ajax({
+      url: 'https://randomuser.me/api/?nat=au,ca,ch,fr,gb,nl,nz,us&results=12&inc=picture,name,email,location,cell,dob',
+      dataType: 'json',
+      success: function(data) {
+          displayInfo(data);
+        console.log(data);
+      }
+    });
+//
+// function searchEmployee(){
+//   //.person is li, p is .name
+//
+//   const input = document.getElementById('search');
+//   let filter = input.value.toLowerCase();
+//   const ul = document.getElementById('myUl');
+//   let li = ul.querySelector('.person')[0];
+//   console.log(li);
+// }
+// searchEmployee();
