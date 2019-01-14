c4358701e59ced27e5cff83b8ceb528f8d73c47f
pskpetya <pskpetya@gmail.com>
2019-01-14 22:53:08 +0400

    working on search/filter

--- js/exceeds.js
+++ js/exceeds.js
@@ -1,22 +1,14 @@
-// ********************************** search/filter
-
-
-
-
-
-
-
-//    var input, filter, ul, li,a, i, txtValue;
-//    input = document.getElementById('search');
-//    filter = input.value.toUpperCase();
-//    ul = document.getElementById('myUl');
-//    li = document.getElementsByTagName('li');
-//    for(i=0; i<li.length; i++){
-//        name = li[i].querySelector('.name');
-//        txtValue = name.textContent || name.innerText;
-//        if(txtValue.toUpperCase().indexOf(filter)>-1) {
-//            li[i].display = '';
-//        } else{
-//            li[i].display = 'none';
-//        }
-//    }
+    var input, filter, ul, li, i, txtValue, name;
+    input = document.getElementById('search');
+    filter = input.value.toUpperCase();
+    ul = document.getElementById('myUl');
+    li = document.getElementsByTagName('li');
+    for(i=0; i<li.length; i++){
+        name = li[i].querySelector('.name');
+        txtValue = name.textContent || name.innerText;
+        if(txtValue.toUpperCase().indexOf(filter)>-1) {
+            li[i].display = '';
+        } else{
+            li[i].display = 'none';
+        }
+    }
