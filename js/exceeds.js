153aae710b5335bfef7853dd18ed6eaf89fe7eb1
pskpetya <pskpetya@gmail.com>
2019-01-16 11:55:33 +0400

    search/filter function added

--- js/exceeds.js
+++ js/exceeds.js
@@ -1,14 +1,22 @@
-    var input, filter, ul, li, i, txtValue, name;
-    input = document.getElementById('search');
-    filter = input.value.toUpperCase();
-    ul = document.getElementById('myUl');
-    li = document.getElementsByTagName('li');
-    for(i=0; i<li.length; i++){
-        name = li[i].querySelector('.name');
-        txtValue = name.textContent || name.innerText;
+
+//loop through all li items and hide the ones not matching the search from the input
+function myFunction(){
+    const input = document.getElementById('search');
+    let filter = input.value.toUpperCase();
+    const ul = document.getElementById('myUl');
+    let li = document.getElementsByTagName('li');
+    
+    // loop through all list items and hide those who dont match the search query
+    for(let i=0; i<li.length; i++){
+        let name = li[i].getElementsByTagName('p')[0];
+        let txtValue = name.textContent || name.innerText;
         if(txtValue.toUpperCase().indexOf(filter)>-1) {
-            li[i].display = '';
+            li[i].style.display = '';
         } else{
-            li[i].display = 'none';
+            li[i].style.display = 'none';
         }
-    }
+  }
+};
+$('input').on('keyup', function(){
+   myFunction(); 
+});
